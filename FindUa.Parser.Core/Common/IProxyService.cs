3b0dad0345774a8a545fb738e35f611552e0d12d
Elijah <illya.suzyma@gmail.com>
2020-07-13 12:18:57 +0300

    Added ProxyService to parser service

---
+++ FindUa.Parser.Core/Common/IProxyService.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace FindUa.Parser.Core.Common
+{
+    public interface IProxyService
+    {
+        string GetRandomProxyUrlFromRedis();
+    }
+}
---
+++ FindUa.RstParser.Domain/Common/ProxyService.cs
@@ -0,0 +1,32 @@
+﻿using CachingFramework.Redis.Contracts;
+using Common.Core.CacheKeys;
+using FindUa.Parser.Core.Common;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace FindUa.RstParser.Domain.Common
+{
+    public class ProxyService : IProxyService
+    {
+        private readonly IContext _redisContext;
+
+        public ProxyService(IContext redisContext)
+        {
+            _redisContext = redisContext;
+        }
+
+        public string GetRandomProxyUrlFromRedis()
+        {
+            var proxiesSet = _redisContext.Collections.GetRedisSet<string>(CacheKeys.Proxy).ToList();
+
+            if (!proxiesSet.Any())
+                throw new WebException("Proxies list is empty");
+
+            var randomizer = new Random();
+            var randomProxyIndex = randomizer.Next(0, proxiesSet.Count - 1);
+
+            return proxiesSet[randomProxyIndex];
+        }
+    }
+}
