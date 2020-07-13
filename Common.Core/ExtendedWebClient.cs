4f3b027f54468605812cffde9fda5bcf12549c34
Elijah <illya.suzyma@gmail.com>
2020-07-13 12:18:25 +0300

    Added ExtendedWebClient.cs

---
+++ Common.Core/ExtendedWebClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Common.Core
+{
+    public class ExtendedWebClient : WebClient
+    {
+        public int Timeout { get; set; } = 10000;
+
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            var w = base.GetWebRequest(uri);
+            w.Timeout = Timeout; //10 seconds timeout
+            return w;
+        }
+
+        public new async Task<string> DownloadStringTaskAsync(Uri address)
+        {
+            var t = base.DownloadStringTaskAsync(address);
+            if (await Task.WhenAny(t, Task.Delay(Timeout)) != t)
+            {
+                CancelAsync();
+                throw new WebException("Connection timeout");
+            }
+            return await t;
+        }
+    }
+}
